e4b0c556633c76234a2a6bb0331f005235bd8656
Humberto Benedito <humbertobenedito@gmail.com>
2023-02-09 19:55:55 +0100

    feat: add first test v1

--- src/PodcastItunes.test.jsx
+++ src/PodcastItunes.test.jsx
@@ -10,37 +10,3 @@ describe('PodcastItunes', () => {
 
   });
 });
-
-// describe("initApp", () => {
-//   const mock = {};
-//   afterEach(cleanup);
-//   beforeEach(() => {
-//     const { container } = render(<PodcastItunes />);
-//     mock.container = container;
-//   });
-
-//   it("Verific header text", () => {
-//     const { container } = mock;
-//     screen.getByText("Itunes Podcaster");
-//     // expect( getByText('Itunes Podcaster') ).toBeInTheDocument();
-//   //   expect(screen.getByRole("combobox")).toHaveDisplayValue("Select a breed");
-//   //   expect(screen.getByRole("button", { name: "Search" })).toBeDisabled();
-//   //   expect(screen.getByRole("img")).toBeInTheDocument();
-//   });
-
-
-
-// });
-
-// describe('true is truthy and false is falsy', () => {
-//   it('true is truthy', () => {
-//     expect(true).toBe(true);
-//   });
-
-//   it('false is falsy', () => {
-//     expect(false).toBe(false);
-//     expect(screen.getByText('Itunes Podcaster')).toBeInTheDocument();
-//   });
-
-
-// });
