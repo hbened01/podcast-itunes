f243ddb7c13326a4a088357b068d28eacd414134
Humberto Benedito <humbertobenedito@gmail.com>
2023-02-09 15:01:05 +0100

    fix: add filters in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,8 @@ import { FaPodcast } from "react-icons/fa";
 const Header = () => {
   return (
     <header className="flex items-center px-4 py-2 bg-gray-600">
-      <FaPodcast className="text-gray-100 text-xl" />
-      <strong className="mx-auto text-gray-100">Itunes Podcaster</strong>
+      <FaPodcast className="text-gray-100 text-xl drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)]" />
+      <strong className="mx-auto text-xl text-gray-100 drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)]">Itunes Podcaster</strong>
     </header>
   );
 };
