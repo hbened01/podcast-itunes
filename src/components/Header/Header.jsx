4803a9d98afbb1a2fac5a36ca00c382bca182c30
Humberto Benedito <humbertobenedito@gmail.com>
2023-02-09 22:36:14 +0100

    feat: add cursor in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header role="heading" className="flex items-center px-4 py-2 bg-gray-600 drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)]">
       <FaPodcast className="text-gray-100 text-xl drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)]" />
-      <strong className="mx-auto text-xl text-gray-100 drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)]">Itunes Podcaster</strong>
+      <strong className="mx-auto text-xl text-gray-100 drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)] hover:text-gray-200 cursor-pointer">Itunes Podcaster</strong>
     </header>
   );
 };
