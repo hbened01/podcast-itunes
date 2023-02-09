adfb8986d97a399147ae064eb4dfdd6f59f5573e
Humberto Benedito <humbertobenedito@gmail.com>
2023-02-09 11:49:54 +0100

    fix: modify css in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,8 @@ import { FaPodcast } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <header className="flex items-center px-4 py-2 bg-gray-500">
-      <FaPodcast className="text-gray-100" />
+    <header className="flex items-center px-4 py-2 bg-gray-600">
+      <FaPodcast className="text-gray-100 text-xl" />
       <strong className="mx-auto text-gray-100">Itunes Podcaster</strong>
     </header>
   );
