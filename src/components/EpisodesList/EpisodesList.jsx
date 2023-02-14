3c3907f386b28ba73f65fd974690fde69af3a290
Humberto Benedito <humbertobenedito@gmail.com>
2023-02-14 08:24:45 +0100

    feat: create episode list component

---
+++ src/components/EpisodesList/EpisodesList.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { BsPlayCircle } from "react-icons/bs"
+import { CiTimer } from "react-icons/ci";
+import { parseISO, format } from "date-fns";
+import "./EpisodesList.scss";
+
+const EpisodesList = ({dataEpisodeTrackCard, dataEpisodes, handleClickEpisode}) => {
+  return (
+    <div className="episodes-container">
+      <div className="episodes-container_header">
+        #Episodes: {dataEpisodeTrackCard?.trackCount}
+      </div>
+      <div className="episodes-container_table">
+        <table>
+          <thead>
+            <tr>
+              <th scope="col">
+                title
+              </th>
+              <th scope="col">
+                date
+              </th>
+              <th scope="col">
+                duration
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              dataEpisodes?.map(({trackName, releaseDate, trackTimeMillis, trackId}, key) =>
+                (
+                  <tr className="bg-white border-b cursor-pointer group" key={key} onClick={() => handleClickEpisode(trackId)}>
+                    <td className="text-blue-500 group-hover:text-blue-700 px-6 py-4 inline-flex whitespace-nowrap"><BsPlayCircle className="text-lg mt-[2px] mr-2" />{trackName}.</td>
+                    <td className="text-gray-500 group-hover:text-gray-700 px-6 py-4 whitespace-nowrap">{format(parseISO(releaseDate), "PPPP")}.</td>
+                    <td className="text-gray-500 group-hover:text-gray-700 px-6 py-4 inline-flex whitespace-nowrap"><CiTimer className="text-lg mt-[2px] mr-2" />{((trackTimeMillis / 1000) / 60).toFixed(0)} Min.</td>
+                  </tr>
+                )
+              )
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default EpisodesList;
--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import NotFound from "./NotFound/NotFound";
 import Search from "./Search/Search";
 import Loader from "./Loader/Loader";
 import BasicCard from "./BasicCard/BasicCard";
+import EpisodesList from "./EpisodesList/EpisodesList";
 
-export { Card, Header, Search, NotFound, Loader, BasicCard };
+export { Card, Header, Search, NotFound, Loader, BasicCard, EpisodesList };
