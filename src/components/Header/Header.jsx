8d575b9fc0759773b4983cc3910380e1177fa3d5
Humberto Benedito <humbertobenedito@gmail.com>
2023-02-14 11:09:47 +0100

    feat: add redirect to home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { FaPodcast } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header role="heading" className="flex items-center px-4 py-2 bg-gray-400 drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)]">
       <FaPodcast className="text-gray-100 text-xl drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)]" />
-      <strong className="mx-auto text-xl text-gray-100 drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)] hover:text-gray-200 cursor-pointer">Itunes Podcaster</strong>
+      <Link to={"/"} className="mx-auto text-xl text-gray-100 drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)] hover:text-gray-200 cursor-pointer">Itunes Podcaster</Link>
     </header>
   );
 };
