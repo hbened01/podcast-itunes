e674fd5a1f06609d826118c05476c3129452e4cd
Humberto Benedito <humbertobenedito@gmail.com>
2023-02-14 11:08:36 +0100

    feat: add to basic card link return

--- src/components/BasicCard/BasicCard.jsx
+++ src/components/BasicCard/BasicCard.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 import "./BasicCard.scss";
 import { randomHex } from "@/utils";
 
 const BasicCard = ({
   collectionName,
+  collectionId,
   artworkUrl600,
   artistName,
   summary,
   genres,
 }) => {
+  const location = useLocation();
   return (
-    <div className="basic-card-container">
+    <Link className="basic-card-container" to={location?.pathname?.includes("podcastDetail") ? `/` : `/podcastDetail/${collectionId}`}>
       <div className="basic-card-container_img">
         <img
           src={artworkUrl600}
@@ -41,7 +44,7 @@ const BasicCard = ({
         </div>
         <div className="basic-card-container_body_description_summary">{summary}</div>
       </div>
-    </div>
+    </Link>
   );
 };
 
