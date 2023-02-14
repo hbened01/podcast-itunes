e35d885796e6f7f0ac0c7b3567db9e7e212ecf91
Humberto Benedito <humbertobenedito@gmail.com>
2023-02-14 08:27:39 +0100

    fix: update limits in service

--- src/services/getPodcastsDetailData.js
+++ src/services/getPodcastsDetailData.js
@@ -2,7 +2,7 @@ import { VITE_API_ITUNES_URL } from "@/constants/";
 
 const getPodcastsDetailData = async (podcastId) => {
   try {
-    const url = `${VITE_API_ITUNES_URL}/lookup?id=${podcastId}&country=US&media=podcast&entity=podcastEpisode&limit=500`;
+    const url = `${VITE_API_ITUNES_URL}/lookup?id=${podcastId}&country=US&media=podcast&entity=podcastEpisode&limit=250`;
     const response = await fetch(
       `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`
     );
