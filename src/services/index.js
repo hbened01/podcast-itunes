b8d1049ee6e190bcbe5a988e5a4840aeed6cc1e4
Humberto Benedito <humbertobenedito@gmail.com>
2023-02-14 19:47:44 +0100

    fix: remove service don`t use

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,3 @@
 import getPodcastsData from "./getPodcastsData";
 import getPodcastsDetailData from "./getPodcastsDetailData";
-import getPodcastsEpisodeDetailData from "./getPodcastsEpisodeDetailData";
-export { getPodcastsData, getPodcastsDetailData, getPodcastsEpisodeDetailData };
+export { getPodcastsData, getPodcastsDetailData };
