f60e675a5b85e332c7d647c0d5cdb0e9907df323
Humberto Benedito <humbertobenedito@gmail.com>
2023-02-15 18:07:42 +0100

    fix: add new docs version

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
+  base: '/podcast_itunes/',
   server: {
     port: 5173,
     host: "podcast_itunes.com",
