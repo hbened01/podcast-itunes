2a39553d77038750dd735a1224279bae2979e5a8
Humberto Benedito <humbertobenedito@gmail.com>
2023-02-15 19:44:11 +0100

    fix: route in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <header role="heading" aria-level="1" className="flex items-center px-4 py-2 bg-gray-400 drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)]">
       <FaPodcast className="text-gray-100 text-xl drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)]" />
-      <Link to={"/"} className="mx-auto text-xl text-gray-100 drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)] hover:text-gray-200 cursor-pointer">Itunes Podcaster</Link>
+      <Link to={"/podcast-itunes"} className="mx-auto text-xl text-gray-100 drop-shadow-[2px_2px_2px_rgba(0,0,0,0.5)] hover:text-gray-200 cursor-pointer">Itunes Podcaster</Link>
       <BiLoaderAlt className="header-loader hidden animate-spin h-5 w-5" />
     </header>
   );
