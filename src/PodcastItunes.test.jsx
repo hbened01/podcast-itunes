46e0622a1fae57fa391d8a2544bd1a81780e0fb7
Humberto Benedito <humbertobenedito@gmail.com>
2023-02-15 08:30:06 +0100

    fix: add memory route to test

--- src/PodcastItunes.test.jsx
+++ src/PodcastItunes.test.jsx
@@ -1,12 +1,16 @@
-import { render, screen, fireEvent, cleanup } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import PodcastItunes from "./PodcastItunes";
+import { MemoryRouter as Router } from "react-router-dom";
 
-describe('PodcastItunes', () => {
-  it('renders headline', () => {
-    render(<PodcastItunes />);
+describe("PodcastItunes", () => {
+  it("renders headline", () => {
+    render(
+      <Router>
+        <PodcastItunes />
+      </Router>
+    );
     // screen.debug();
-    expect(screen.getByText('Itunes Podcaster')).toBeInTheDocument();
-    expect(screen.getByRole("heading")).toBeInTheDocument()
-
+    expect(screen.getByText("Itunes Podcaster")).toBeInTheDocument();
+    expect(screen.getByRole("heading")).toBeInTheDocument();
   });
 });
--- src/views/PodcastDetail/PodcastDetail.jsx
+++ src/views/PodcastDetail/PodcastDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { getPodcastsDetailData } from "@/services";
+import { getPodcastsDetailData, getPodcastsData } from "@/services";
 import { addHours, getTime } from "date-fns";
 import { BasicCard, Loader, EpisodesList } from "@/components";
 import "./PodcastDetail.scss";
@@ -10,22 +10,22 @@ const PodcastDetail = () => {
   const navigate = useNavigate();
   const [dataEpisodes, setDataEpisodes] = useState([]);
   const [dataPodcastTrackCard, setDataPodcastTrackCard] = useState([]);
-  const once = useRef(true); // Fix twice calls in hook useEffect.
   const [isLoading, setIsLoading] = useState(false);
+  const [summary, setSummary] = useState(null);
+  const once = useRef(true); // Fix twice calls in hook useEffect.
   const headerLoader = useRef(null);
+  const podcastDataStorage = useRef(null);
   const podcastsEpisodesDataStorage = useRef(null);
 
   podcastsEpisodesDataStorage.current = JSON.parse(
     window.localStorage.getItem("DATA_PODCASTS_EPISODES")
   );
 
-  // const { summary } = podcastDataStorage;
-
-  // const setdataEpisodesInStates = (episode) => {
-  //   episode?.wrapperType !== "track"
-  //     ? setDataEpisodes((prevState) => [...prevState, episode])
-  //     : setDataPodcastTrackCard(episode);
-  // };
+  podcastDataStorage.current = JSON.parse(
+    window.localStorage.getItem("DATA_PODCASTS_HOME")
+  )?.dataListPodcasts?.find(
+    (podcast) => podcast?.id?.attributes["im:id"] === podcastId
+  );
 
   const setdataEpisodesInStates = (episode) => {
     episode?.wrapperType !== "track"
@@ -43,8 +43,40 @@ const PodcastDetail = () => {
   };
 
   useEffect(() => {
+
     // Save reference header loader:
     headerLoader.current = document.querySelector(".header-loader");
+
+    // Set Summary if exists:
+    podcastDataStorage.current &&
+      setSummary(podcastDataStorage.current?.summary?.label);
+
+    // Get data if not available in the local storage:
+    !podcastsEpisodesDataStorage.current &&
+      getPodcastsData()
+        .then((data) => {
+          const dataFetchPodcaster = data?.feed?.entry;
+          const dateControlApiTime = getTime(addHours(new Date(), 24)); // CONTROL API TIME 24 HRS.
+          const podcastData = dataFetchPodcaster.find(
+            (podcast) => podcast?.id?.attributes["im:id"] === podcastId
+          );
+          // SET DATA IN THE STATE:
+          podcastDataStorage.current = podcastData;
+          // SET DATA IN LOCALSTORAGE:
+          window.localStorage.setItem(
+            "DATA_PODCASTS_HOME",
+            JSON.stringify({
+              dateControl: dateControlApiTime,
+              dataListPodcasts: dataFetchPodcaster,
+            })
+          );
+          // Set summary:
+          setSummary(
+            podcastData?.summary?.label
+          );
+        })
+        .catch((error) => console.log(error));
+
     if (once.current) {
       once.current = false;
       // Add loading indicator:
@@ -58,13 +90,14 @@ const PodcastDetail = () => {
         !podcastsEpisodesDataStorage.current ||
         isMatchIndex === -1 ||
         getTime(new Date()) >
-        podcastsEpisodesDataStorage.current[isMatchIndex]?.dateControl
+          podcastsEpisodesDataStorage.current[isMatchIndex]?.dateControl
       ) {
         // REMOVE COLLECTION ID BEFORE UPDATE WITH NEW INSTANCE.
         if (typeof isMatchIndex !== "undefined" && isMatchIndex !== -1) {
-          podcastsEpisodesDataStorage.current = podcastsEpisodesDataStorage.current?.filter(
-            (episode) => episode.collectionId !== podcastId
-          );
+          podcastsEpisodesDataStorage.current =
+            podcastsEpisodesDataStorage.current?.filter(
+              (episode) => episode.collectionId !== podcastId
+            );
         }
         getPodcastsDetailData(podcastId)
           .then((data) => {
@@ -109,11 +142,12 @@ const PodcastDetail = () => {
       }, 1000);
     }
   }, [podcastId]);
+
   return (
     <>
       {!isLoading && (
         <div className="podcast-detail-container">
-          <BasicCard summary={dataPodcastTrackCard?.summary?.label} {...dataPodcastTrackCard} />
+          <BasicCard summary={summary} {...dataPodcastTrackCard} />
           <EpisodesList
             dataPodcastTrackCard={dataPodcastTrackCard}
             dataEpisodes={dataEpisodes}
